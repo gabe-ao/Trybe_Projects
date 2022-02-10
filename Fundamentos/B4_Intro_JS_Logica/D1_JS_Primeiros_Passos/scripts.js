49ba7a359d5e144aeb1eaf44633be7add2bf216e
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2022-02-10 18:32:16 -0300

    Primeiros exercicios de JS B4-D1

---
+++ Fundamentos/B4_Intro_JS_Logica/D1_JS_Primeiros_Passos/scripts.js
@@ -0,0 +1,17 @@
+let myName = "Gabriel";
+let birthCity = "Fortaleza";
+let birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge);
